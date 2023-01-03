45934ffb67a36016a69f1b87c295b1f7a9bbf135
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-01-04 02:09:01 +0530

    Create Water Consumption.py

---
+++ 0 - 1000: Beginner Level/Water Consumption.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/WATERCONS
+
+Code : 
+
+T = int(input())
+for i in range(T):
+    x = int(input())
+    if x >= 2000:
+        print('YES')
+    else:
+        print('NO')
