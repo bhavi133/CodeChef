f928218e3f128ee619e369ae52519f807f2c5f15
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-01-04 13:35:54 +0530

    Create Program Your Own CALCULATOR.py

---
+++ 1000 - 1200: 1* Beginner Level/Program Your Own CALCULATOR.py
@@ -0,0 +1,15 @@
+Link : https://www.codechef.com/problems/URCALC
+
+Code :
+
+a = int(input())
+b = int(input())
+c = input()
+if c == '+':
+    print(a+b)
+elif c == '-':
+    print(a-b)
+elif c == '*':
+    print(a*b)
+else:
+    print(a/b)
