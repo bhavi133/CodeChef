16d0c0a12790d5f12afc02cb3d58e28abecb023d
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-01-09 15:14:12 +0530

    Create Get Subscription.py

---
+++ 0 - 1000: Beginner Level/Get Subscription.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/SUBSCRIBE
+
+Code :
+
+T = int(input())
+for i in range(T):
+    x = int(input())
+    if x > 30:
+        print('YES')
+    else:
+        print('NO')
