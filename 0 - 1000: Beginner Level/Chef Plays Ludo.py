de98d067f4ab9612e427d3240e41632484c338ea
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-01-12 14:25:29 +0530

    Create Chef Plays Ludo.py

---
+++ 0 - 1000: Beginner Level/Chef Plays Ludo.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/LUDO
+
+Code :
+
+T = int(input())
+for i in range(T):
+    x = int(input())
+    if x == 6:
+        print('YES')
+    else:
+        print('NO')
