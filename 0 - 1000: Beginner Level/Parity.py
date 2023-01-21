e62ca91c4416eee6aae0d14a4f0ac8a20fcefbad
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-01-22 01:12:51 +0530

    Create Parity.py

---
+++ 0 - 1000: Beginner Level/Parity.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/PAR2
+
+Code :
+
+T = int(input())
+for i in range(T):
+    n = int(input())
+    if n % 2 == 0:
+        print('YES')
+    else:
+        print('NO')
