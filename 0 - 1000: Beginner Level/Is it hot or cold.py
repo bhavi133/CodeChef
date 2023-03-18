f065ceacea05debfb258568a40e6d7451b999efd
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-03-18 19:16:48 +0530

    Create Is it hot or cold.py

---
+++ 0 - 1000: Beginner Level/Is it hot or cold.py
@@ -0,0 +1,10 @@
+Link : https://www.codechef.com/problems/HOTCOLD
+
+Code :
+
+for i in range(int(input())):
+    x = int(input())
+    if x > 20:
+        print('HOT')
+    else:
+        print('COLD')
