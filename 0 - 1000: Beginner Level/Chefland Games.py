0a180bfc816c2ed59040c97ad6c11acb5d2e7c94
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-03-22 17:42:53 +0530

    Create Chefland Games.py

---
+++ 0 - 1000: Beginner Level/Chefland Games.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/CHEFGAMES
+
+Code :
+
+T = int(input())
+for i in range(T):
+    R1, R2, R3, R4 = map(int, input().split())
+    if R1 == 0 and R2 == 0 and R3 == 0 and R4 == 0:
+        print('IN')
+    else:
+        print('OUT')
