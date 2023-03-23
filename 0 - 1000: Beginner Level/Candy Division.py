204c51ab1ef098c6dc135dbc197ee61e97427d0b
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-03-23 18:45:32 +0530

    Create Candy Division.py

---
+++ 0 - 1000: Beginner Level/Candy Division.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/CANDIVIDE
+
+Code :
+
+T = int(input())
+for i in range(T):
+    X = int(input())
+    if X % 3 == 0:
+        print('YES')
+    else:
+        print('NO')
