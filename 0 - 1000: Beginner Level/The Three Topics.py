31739131878a3d9466e712b04ab472421dd4a3ee
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-03-24 22:01:36 +0530

    Create The Three Topics.py

---
+++ 0 - 1000: Beginner Level/The Three Topics.py
@@ -0,0 +1,9 @@
+Link : https://www.codechef.com/problems/THREETOPICS
+
+Code :
+
+a, b, c, x = map(int, input().split())
+if (x == a) or (x == b) or (x == c):
+    print('YES')
+else:
+    print('NO')
