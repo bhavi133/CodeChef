b4d5882bccc0139a031afc3087ae7e2f1e3ba6e0
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-03-28 15:46:37 +0530

    Create Nearest Exit.py

---
+++ 0 - 1000: Beginner Level/Nearest Exit.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/NEARESTEXIT
+
+Code :
+
+T = int(input())
+for i in range(T):
+    X = int(input())
+    if X <= 50:
+        print("LEFT")
+    else:
+        print("RIGHT")
