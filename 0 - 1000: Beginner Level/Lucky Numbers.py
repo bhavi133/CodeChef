69751ea99b4fc92562611dda70aaa8ef1e828f2b
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-04-15 16:45:52 +0530

    Create Lucky Numbers.py

---
+++ 0 - 1000: Beginner Level/Lucky Numbers.py
@@ -0,0 +1,11 @@
+Link : https://www.codechef.com/problems/LUCKYN
+
+Code :
+
+T = int(input())
+for i in range(T):
+    X = int(input())
+    if '7' in str(X):
+        print('YES')
+    else:
+        print('NO')
