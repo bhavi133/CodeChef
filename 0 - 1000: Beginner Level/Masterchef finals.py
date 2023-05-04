a59e91f1c6649dcd640721b9cf5ec60477419185
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-05-04 20:50:27 +0530

    Create Masterchef finals.py

---
+++ 0 - 1000: Beginner Level/Masterchef finals.py
@@ -0,0 +1,10 @@
+Link : https://www.codechef.com/problems/TOP10
+
+Code :
+
+for i in range(int(input())):
+    x = int(input())
+    if x <= 10:
+        print('YES')
+    else:
+        print('NO')
